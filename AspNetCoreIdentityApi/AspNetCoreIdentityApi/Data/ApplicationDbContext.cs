dc53559c2f3ec2d4fd86799024e955cdcd04fa8d
Breno Assis <breno.assis9@outlook.com.br>
2025-03-26 08:13:28 -0300

    Adicionando configuração inicial do Identity e banco de dados sql server

---
+++ AspNetCoreIdentityApi/AspNetCoreIdentityApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentityApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+    }
+
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
--- AspNetCoreIdentityApi/AspNetCoreIdentityApi/Program.cs
+++ AspNetCoreIdentityApi/AspNetCoreIdentityApi/Program.cs
@@ -1,6 +1,25 @@
+using AspNetCoreIdentityApi.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do banco de dados
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Adicionando o serviço do Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configuração do pipeline HTTP
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
